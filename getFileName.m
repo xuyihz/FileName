8ddf97234b8d159144af2f481dd9f866d31c5bde
Xu Yi <xuyi@ziad.cn>
2019-09-28 16:04:40 +0800

    works

--- getFileName.m
+++ getFileName.m
@@ -1,15 +1,16 @@
 %% get File name, and export to excel
-% 
-% 
-% 
+% 把特定后缀名的文件名提取到excel中
+% 更改cd命令后''中的路径
+%
 % Xu Yi, 2019
 
 %%
 close all; clear; clc;
 
 %%
+cd ''; % 更改当前文件夹
 suffix = 'dwf';     % 需要识别的文件（通过后缀名识别）
-filename = 'dirName.xlsx';     % 导出的文件名
+filename = 'dName.xlsx';     % 导出的文件名
 
 mydir = dir(['*.', suffix]); % 仅识别.suffix的文件
 mydirCell = struct2cell(mydir);
@@ -17,7 +18,9 @@ mydirNameTemp = mydirCell(1,:); % 提取name
 
 mydirName = {0}; DrawNum = {0}; DrawName = {0}; % 初始化
 for i = 1:length(mydirNameTemp)
-    mydirName{i,1} = strtok(mydirNameTemp{i},'.'); % 提取文件名中'.'之前的内容，即去掉后缀
+    nameTemp = mydirNameTemp{i};
+    nameTemp( end-length(suffix):end ) = []; % 删除后缀名.suffix
+    mydirName{i,1} = nameTemp;
     [DrawNum{i,1}, DrawName{i,1}] = strtok(mydirName{i,1}, '_'); % 分开图号与图名
     DrawName{i,1} = DrawName{i,1}(2:end); % 删除'_'
 end
--- reName.m
+++ reName.m
@@ -1,23 +1,29 @@
 %% rename Files
-%
-%
-%
+% filename = 'dName.xlsx' 这里的文件名中不包含文件后缀名
+% 如需仅更改后缀名, 运行system('rename *.oldSuffix *.newSuffix')
+% 更改cd命令后''中的路径
 % Xu Yi, 2019
 
 %%
 close all; clear; clc;
 
 %%
-suffix = 'dwf';     % 需要识别的文件（通过后缀名识别）
-mydir = dir(['*.', suffix]); % 仅识别.suffix的文件
+cd ''; % 更改当前文件夹
+oldSuffix = 'dwf';     % 源文件后缀名
+newSuffix = 'dwf';     % 需要更改后的后缀名
+filename = 'dName.xlsx';     % 读取的文件名
 
+mydir = dir(['*.', oldSuffix]); % 仅识别.oldSuffix的文件
+mydirCell = struct2cell(mydir);
+oldName = mydirCell(1,:); % 提取name
+newName = readcell(filename);
 for i = 1:length(mydir)
-    oldname = files(i).name;
-    newname = strcat('RH_', oldname);
-    command = ['rename' 32 oldname 32 newname]; % 32 是 ASCII 码，表示空格。
-    status = dos(command); % 这个函数调用执行操作系统命令
-    if status == 0
-    else
-        disp([oldname, '重命名失败!\n'])
+    command = ['rename', ' "', oldName{i}, '" "',...
+        newName{i,1}, '.', newSuffix, '"'];
+    % rename [<Drive>:][<Path>]<FileName1> <FileName2>
+    % 文件名中有空格时，需要在路径外加""
+    status = system(command); % 这个函数调用执行操作系统命令
+    if status ~= 0
+        disp([oldName{i}, '重命名失败。\n'])
     end
 end
