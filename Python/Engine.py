1d428aee6d02e4a2de7cf760c778813bd7725628
Xu Yi <xuyi@ziad.cn>
2020-02-04 13:57:28 +0800

    import os

---
+++ Python/Engine.py
@@ -0,0 +1,10 @@
+# Engine class
+# Xu Yi
+# 2020
+
+class Engine(object):
+
+    def __init__(self):
+        pass
+
+    
--- Python/ex.py
+++ Python/ex.py
@@ -3,7 +3,7 @@ import FileName as FN
 path = "C:/Users/XYGC/Desktop/FileName/Python/ex.py"
 a = FN.FileName(path)
 
-print(a.path)
-print(a.folder)
-print(a.name)
-print(a.suffix)
+print("path = ", a.path)
+print("DirName = ", a.DirName)
+print("Name = ", a.Name)
+print("Extension = ", a.Extension)
