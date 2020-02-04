09b7574e838d525617a842cef2026c6d5b61dc05
Xu Yi <xuyi@ziad.cn>
2020-02-04 23:14:58 +0800

    2020.2.4

--- Python/Engine.py
+++ Python/Engine.py
@@ -2,9 +2,11 @@
 # Xu Yi
 # 2020
 
+import json
+
+
 class Engine(object):
 
     def __init__(self):
         pass
-
     
--- Python/FileName.py
+++ Python/FileName.py
@@ -7,8 +7,9 @@ import os
 
 class FileName(object):
 
-    def __init__(self, path):
+    def __init__(self, path, namesep):
         self.path = path
         self.DirName = os.path.dirname(path)
         basename = os.path.basename(path)
         (self.Name, self.Extension) = os.path.splitext(basename)
+        self.NameSplit = self.Name.split(namesep)
