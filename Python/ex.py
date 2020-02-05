fe8d97768d140d26f4f84f66d6ec312a19dcc106
Xu Yi <xuyi@ziad.cn>
2020-02-05 17:00:45 +0800

    move license file

--- Python/ex.py
+++ Python/ex.py
@@ -2,7 +2,7 @@ from Engine import Engine as EG
 import os
 
 
-ext = '.txt'
+ext = '.py'
 namesep = '_'
 Dirname = os.path.dirname(__file__)
 EGobj = EG(Dirname)
@@ -11,4 +11,4 @@ EGobj = EG(Dirname)
 EGobj.genFNJson(ext, namesep)
 
 # rename
-EGobj.reJsonFN(ext, namesep)
+# EGobj.reJsonFN(ext, namesep)
