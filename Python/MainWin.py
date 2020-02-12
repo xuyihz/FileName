34b4e6133aaa8f8e26c3cc4ed0fdb01095beb889
Xu Yi <xuyi@ziad.cn>
2020-02-12 23:26:30 +0800

    Qtableview

--- Python/MainWin.py
+++ Python/MainWin.py
@@ -20,47 +20,54 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(456, 453)
+        MainWindow.resize(709, 500)
+        font = QFont()
+        font.setFamily(u"Arial")
+        font.setPointSize(12)
+        MainWindow.setFont(font)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.pushButton_dir = QPushButton(self.centralwidget)
         self.pushButton_dir.setObjectName(u"pushButton_dir")
-        self.pushButton_dir.setGeometry(QRect(30, 20, 81, 31))
+        self.pushButton_dir.setGeometry(QRect(30, 30, 141, 61))
+        self.pushButton_dir.setFont(font)
         self.pushButton_rename = QPushButton(self.centralwidget)
         self.pushButton_rename.setObjectName(u"pushButton_rename")
-        self.pushButton_rename.setGeometry(QRect(330, 70, 81, 31))
+        self.pushButton_rename.setGeometry(QRect(540, 120, 141, 61))
+        self.pushButton_rename.setFont(font)
         self.pushButton_loadjson = QPushButton(self.centralwidget)
         self.pushButton_loadjson.setObjectName(u"pushButton_loadjson")
-        self.pushButton_loadjson.setGeometry(QRect(130, 70, 81, 31))
+        self.pushButton_loadjson.setGeometry(QRect(200, 120, 141, 61))
+        self.pushButton_loadjson.setFont(font)
         self.pushButton_writejson = QPushButton(self.centralwidget)
         self.pushButton_writejson.setObjectName(u"pushButton_writejson")
-        self.pushButton_writejson.setGeometry(QRect(30, 70, 81, 31))
+        self.pushButton_writejson.setGeometry(QRect(30, 120, 141, 61))
+        self.pushButton_writejson.setFont(font)
         self.CloseWinBtn = QPushButton(self.centralwidget)
         self.CloseWinBtn.setObjectName(u"CloseWinBtn")
-        self.CloseWinBtn.setGeometry(QRect(30, 300, 381, 31))
+        self.CloseWinBtn.setGeometry(QRect(30, 410, 651, 41))
+        self.CloseWinBtn.setFont(font)
         self.textBrowser_dir = QTextBrowser(self.centralwidget)
         self.textBrowser_dir.setObjectName(u"textBrowser_dir")
-        self.textBrowser_dir.setGeometry(QRect(130, 20, 281, 31))
-        self.textBrowser_src = QTextBrowser(self.centralwidget)
-        self.textBrowser_src.setObjectName(u"textBrowser_src")
-        self.textBrowser_src.setGeometry(QRect(30, 110, 181, 21))
-        self.textBrowser_dst = QTextBrowser(self.centralwidget)
-        self.textBrowser_dst.setObjectName(u"textBrowser_dst")
-        self.textBrowser_dst.setGeometry(QRect(230, 110, 181, 21))
+        self.textBrowser_dir.setGeometry(QRect(200, 30, 481, 61))
+        self.textBrowser_dir.setFont(font)
         self.lineEdit_ext = QLineEdit(self.centralwidget)
         self.lineEdit_ext.setObjectName(u"lineEdit_ext")
-        self.lineEdit_ext.setGeometry(QRect(230, 70, 81, 31))
+        self.lineEdit_ext.setGeometry(QRect(370, 120, 141, 61))
+        self.lineEdit_ext.setFont(font)
         self.lineEdit_ext.setAlignment(Qt.AlignCenter)
         self.tableView_src = QTableView(self.centralwidget)
         self.tableView_src.setObjectName(u"tableView_src")
-        self.tableView_src.setGeometry(QRect(30, 140, 181, 141))
+        self.tableView_src.setGeometry(QRect(30, 210, 311, 171))
+        self.tableView_src.setFont(font)
         self.tableView_dst = QTableView(self.centralwidget)
         self.tableView_dst.setObjectName(u"tableView_dst")
-        self.tableView_dst.setGeometry(QRect(230, 140, 181, 141))
+        self.tableView_dst.setGeometry(QRect(370, 210, 311, 171))
+        self.tableView_dst.setFont(font)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 456, 22))
+        self.menubar.setGeometry(QRect(0, 0, 709, 24))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -72,7 +79,7 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Rename File", None))
         self.pushButton_dir.setText(QCoreApplication.translate("MainWindow", u"find dirpath", None))
         self.pushButton_rename.setText(QCoreApplication.translate("MainWindow", u"rename file", None))
         self.pushButton_loadjson.setText(QCoreApplication.translate("MainWindow", u"load json", None))
@@ -81,18 +88,8 @@ class Ui_MainWindow(object):
         self.textBrowser_dir.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:'SimSun'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
-        self.textBrowser_src.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:'SimSun'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">file name (source)</p></body></html>", None))
-        self.textBrowser_dst.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:'SimSun'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">file name (destination)</p></body></html>", None))
+"</style></head><body style=\" font-family:'Arial','Arial'; font-size:12pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'SimSun'; font-size:9pt;\"><br /></p></body></html>", None))
         self.lineEdit_ext.setText(QCoreApplication.translate("MainWindow", u"ext", None))
     # retranslateUi
 
--- Python/MainWinCon.py
+++ Python/MainWinCon.py
@@ -21,16 +21,19 @@ class Ui_MainWindow(MWU):
         super(Ui_MainWindow, self).setupUi(MainWindow)
         self.ModelObj.ext_dst = self.lineEdit_ext.text()
 
-        self.pushButton_dir.clicked.connect(self.openFile)  # find the feature file
+        self.pushButton_dir.clicked.connect(
+            self.openFile)  # find the feature file
         self.pushButton_rename.clicked.connect(self.renameFile)  # rename
         self.pushButton_loadjson.clicked.connect(self.loadJson)  # load json
-        self.pushButton_writejson.clicked.connect(self.ModelObj.genFNJson)  # write json        
+        self.pushButton_writejson.clicked.connect(
+            self.ModelObj.genFNJson)  # write json
         self.CloseWinBtn.clicked.connect(MainWindow.close)  # exit
 
         self.lineEdit_ext.editingFinished.connect(self.changeExtDst)
 
     def openFile(self):
-        openfileName, openfileType = QtWidgets.QFileDialog.getOpenFileName(None,'choose a file','','')
+        openfileName, openfileType = QtWidgets.QFileDialog.getOpenFileName(
+            None, 'choose a file', '', '')
         self.ModelObj.Fdir = os.path.dirname(openfileName)
         self.ModelObj.Jdir = self.ModelObj.Fdir
         self.textBrowser_dir.setText(self.ModelObj.Fdir)
@@ -41,13 +44,34 @@ class Ui_MainWindow(MWU):
         self.ModelObj.ext_dst = Extension  # assume ext_dst == ext_src
         self.lineEdit_ext.setText(self.ModelObj.ext_dst[1:])
 
-        # self.tableView_src
+        FNList = self.ModelObj.getFNList()
+        self.FileNameTable(self.tableView_src, FNList,
+                           self.ModelObj.ext_src, 'src Name')
 
     def loadJson(self):
         self.ModelObj.FNList = self.ModelObj.J2FNList()
+        self.FileNameTable(self.tableView_dst, self.ModelObj.FNList,
+                           self.ModelObj.ext_dst, 'dst Name')
 
     def renameFile(self):
         self.ModelObj.reJsonFN()
 
     def changeExtDst(self):
         self.ModelObj.ext_dst = '.' + self.lineEdit_ext.text()  # get the text from lineEdit
+        self.FileNameTable(self.tableView_dst, self.ModelObj.FNList,
+                           self.ModelObj.ext_dst, 'dst Name')
+
+    def FileNameTable(self, TableView, FNList, ext, Header):
+        # create Model
+        self.IM = QtGui.QStandardItemModel()
+        # set Horizontal Header
+        self.IM.setHorizontalHeaderItem(0, QtGui.QStandardItem(Header))
+        # set Item
+        for i in range(len(FNList)):
+            self.IM.setItem(i, QtGui.QStandardItem(FNList[i]['Name']+ext))
+        # sort
+        # self.IM.sort(1, QtCore.Qt.DescendingOrder)
+        # set Model
+        TableView.setModel(self.IM)
+        # set Section ResizeMode(Stretch)
+        TableView.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
